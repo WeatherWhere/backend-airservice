faded6e56355fc6d1c40e6a3626ca82fe2f3ce2c
j2woo <kjk04021@naver.com>
2023-03-31 21:03:56 +0900

    (fix: airForecast api path)대기 주간예보 path 수정

--- src/main/java/com/weatherwhere/airservice/controller/airforecast/AirForecastController.java
+++ src/main/java/com/weatherwhere/airservice/controller/airforecast/AirForecastController.java
@@ -18,7 +18,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
-@RequestMapping("air/forecast")
+@RequestMapping("/air/forecast")
 @Slf4j
 @RequiredArgsConstructor
 public class AirForecastController {
