97ba87d1596d6c83226ea800b9c0ffead8185de1
it's U <40010878+j2woo@users.noreply.github.com>
2023-04-07 17:26:37 +0900

    (feat: tourApi) 관광에서 사용할 api (#19)
    * (feat: tourApi) 관광에서 사용할 api
    
    * (refactor: Code) 코드 정리

--- src/main/java/com/weatherwhere/airservice/service/airforecast/AirForecastApiService.java
+++ src/main/java/com/weatherwhere/airservice/service/airforecast/AirForecastApiService.java
@@ -1,17 +1,18 @@
 package com.weatherwhere.airservice.service.airforecast;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import org.json.simple.parser.ParseException;
 
 import com.weatherwhere.airservice.domain.airforecast.AirForecastEntity;
 import com.weatherwhere.airservice.domain.airforecast.AirForecastId;
 import com.weatherwhere.airservice.dto.airforecast.AirForecastDto;
-import com.weatherwhere.airservice.dto.ResultDto;
+import com.weatherwhere.airservice.dto.ResultDTO;
 
 public interface AirForecastApiService {
     // 대기 주간예보 api 데이터 받아오는 메서드
-    ResultDto<Object> getApiData(LocalDate date) throws ParseException, java.text.ParseException;
+    ResultDTO<List<AirForecastDto>> getApiData(LocalDate date) throws ParseException, java.text.ParseException;
 
     // DTO -> Entity
     default AirForecastEntity toEntity(AirForecastDto dto){
--- src/main/java/com/weatherwhere/airservice/service/airforecast/GetAirForecastDataService.java
+++ src/main/java/com/weatherwhere/airservice/service/airforecast/GetAirForecastDataService.java
@@ -1,16 +1,20 @@
 package com.weatherwhere.airservice.service.airforecast;
 
+import java.util.List;
+
 import com.weatherwhere.airservice.domain.airforecast.AirForecastEntity;
 import com.weatherwhere.airservice.dto.airforecast.AirForecastDto;
 import com.weatherwhere.airservice.dto.airforecast.SearchAirForecastDto;
-import com.weatherwhere.airservice.dto.ResultDto;
+import com.weatherwhere.airservice.dto.ResultDTO;
 
 public interface GetAirForecastDataService {
 
-    // 해당 위치 7일 대기오염 주간예보 DB 가져오기
+    // 해당 위치 5일 대기오염 주간예보 DB 가져오기
     // startDate에서 필요한 형식은 yyyy-MM-dd
+    ResultDTO<List<AirForecastDto>> getFiveDaysDataOfLocation(SearchAirForecastDto searchAirForecastDto) throws Exception;
 
-    ResultDto<Object> getSevenDaysDataOfLocation(SearchAirForecastDto searchAirForecastDto) throws Exception;
+    // 하루 주간예보 가져오기
+    ResultDTO<AirForecastDto> getAirForecastOneDay(SearchAirForecastDto searchAirForecastDto);
 
 
     // Entity->DTO
