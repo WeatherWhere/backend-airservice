99cf12c355a28c5e1ec86dad313bb325d8e21320
j2woo <kjk04021@naver.com>
2023-04-10 23:27:33 +0900

    (refactor: Code) code 수정

--- src/main/java/com/weatherwhere/airservice/service/GetTmXYAndStationService.java
+++ src/main/java/com/weatherwhere/airservice/service/GetTmXYAndStationService.java
@@ -1,8 +1,6 @@
 package com.weatherwhere.airservice.service;
 
-import org.json.simple.parser.ParseException;
-
 public interface GetTmXYAndStationService {
     // 경도 x, 위도 y로 측정소명 받기
-    String getStationName(Double x, Double y) throws ParseException;
+    String getStationName(Double x, Double y);
 }
