e2128478e83406baf10a346281f8537669c254ea
j2woo <kjk04021@naver.com>
2023-04-21 12:03:39 +0900

    (fix: path) 실시간 배치 path 수정

--- src/main/java/com/weatherwhere/airservice/controller/airrealtime/RealTimeAirBatchContreoller.java
+++ src/main/java/com/weatherwhere/airservice/controller/airrealtime/RealTimeAirBatchContreoller.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/batch")
+@RequestMapping("/air/batch")
 @RequiredArgsConstructor
 public class RealTimeAirBatchContreoller {
     private final RealTimeAirBatchConfiguration realTimeAirBatchConfiguration;
